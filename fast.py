a267586961e78aa3ce80b53826c322c8c1515894
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-04-04 12:55:57 +0500

    Update fast.py

--- fast.py
+++ fast.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 @app.get('/')
-def main():
+def myapi():
     agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0"
     queries = ['1234']
     try:
