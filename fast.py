33ed7c8b6f592f513aee5dd37c06f2eade591b1e
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-04-06 13:52:57 +0500

    Update fast.py

--- fast.py
+++ fast.py
@@ -11,40 +11,42 @@ def myapi():
     try:
         current = "started"
         for query in queries:
-            def getData(query, id, agent,event_valid,view_state,view_state_gen):
-                cookies = {
-                    'ASP.NET_SessionId': id,
-                    'DISCLAIMER': '1',
-                }
-
-                headers = {
-                    'User-Agent': agent,
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-                    'Accept-Language': 'en-US,en;q=0.5',
-                    'Accept-Encoding': 'gzip, deflate, br',
-                    'Referer': 'https://auditor.ashtabulacounty.us/PT/search/commonsearch.aspx?mode=parid',
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Origin': 'https://auditor.ashtabulacounty.us',
-                    'DNT': '1',
-                    'Connection': 'keep-alive',
-                    # Requests sorts cookies= alphabetically
-                    # 'Cookie': 'ASP.NET_SessionId=ibhnx5dach3twfljh3fw4ed0; DISCLAIMER=1',
-                    'Upgrade-Insecure-Requests': '1',
-                    'Sec-Fetch-Dest': 'document',
-                    'Sec-Fetch-Mode': 'navigate',
-                    'Sec-Fetch-Site': 'same-origin',
-                    'Sec-Fetch-User': '?1',
-                    'Pragma': 'no-cache',
-                    'Cache-Control': 'no-cache',
-                }
-
+            def getData(query,event_valid,view_state,view_state_gen):
                 params = {
                     'mode': 'PARID',
                 }
 
-                data = f'ScriptManager1_TSM=%3B%3BAjaxControlToolkit%2C+Version%3D4.1.50731.0%2C+Culture%3Dneutral%2C+PublicKeyToken%3D28f01b0e84b6d53e%3Aen-US%3Af8fb2a65-e23a-483b-b20e-6db6ef539a22%3Aea597d4b%3Ab25378d2%3BTelerik.Web.UI%2C+Version%3D2017.3.913.45%2C+Culture%3Dneutral%2C+PublicKeyToken%3D121fae78165ba3d4%3Aen-US%3A03e3fdef-45f6-40a0-88ab-9645d53a0f37%3A16e4e7cd%3A33715776%3A58366029%3Af7645509%3A24ee1bba%3Af46195d3%3A874f8ea2%3Ab2e06756%3A92fe8ea0%3Afa31b949%3A4877f69a%3Ac128760b%3A19620875%3A490a9d4e&__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE={view_state}&__VIEWSTATEGENERATOR={view_state_gen}&__EVENTVALIDATION={event_valid}&PageNum=&SortBy=PARID&SortDir=+asc&PageSize=15&hdAction=Search&hdIndex=&sIndex=-1&hdListType=PA&hdJur=&hdSelectAllChecked=false&inpParid={query}&selSortBy=PARID&selSortDir=+asc&selPageSize=15&searchOptions%24hdBeta=&btSearch=&RadWindow_NavigateUrl_ClientState=&mode=PARID&mask=&param1=&searchimmediate='
+                data = {
+                        '__EVENTTARGET': '',
+                        '__EVENTARGUMENT': '',
+                        '__VIEWSTATE': view_state,
+                        '__VIEWSTATEGENERATOR': view_state_gen,
+                        '__EVENTVALIDATION': event_valid,
+                        'PageNum': '',
+                        'SortBy': 'PARID',
+                        'SortDir': ' asc',
+                        'PageSize': '500',
+                        'hdAction': 'Search',
+                        'hdIndex': '',
+                        'sIndex': '-1',
+                        'hdListType': 'PA',
+                        'hdJur': '',
+                        'hdSelectAllChecked': 'false',
+                        'inpParid': query,
+                        'selSortBy': 'PARID',
+                        'selSortDir': ' asc',
+                        'selPageSize': '500',
+                        'searchOptions$hdBeta': '',
+                        'btSearch': '',
+                        'RadWindow_NavigateUrl_ClientState': '',
+                        'mode': 'PARID',
+                        'mask': '',
+                        'param1': '',
+                        'searchimmediate': '',
+}
 
-                response = session.post('https://auditor.ashtabulacounty.us/PT/search/CommonSearch.aspx', headers=headers, params=params, cookies=cookies, data=data)
+
+                response = session.post('https://auditor.ashtabulacounty.us/PT/search/CommonSearch.aspx', params=params, data=data)
 
                 parse = response.text
                 parsed = soap(parse,'lxml')
@@ -156,42 +158,38 @@ def myapi():
             view_state_gen = view_state_gen["value"]
             event_valid =page.find("input", {'name' : "__EVENTVALIDATION"})
             event_valid = event_valid["value"]
-            print(view_state)
-            print(view_state_gen)
-            print(event_valid)
-            current = "third"
             if len(str(query)) != 12:
                 params = {
                     'mode': 'PARID',
                 }
 
                 data = {
-    '__EVENTTARGET': '',
-    '__EVENTARGUMENT': '',
-    '__VIEWSTATE': view_state,
-    '__VIEWSTATEGENERATOR': view_state_gen,
-    '__EVENTVALIDATION': event_valid,
-    'PageNum': '',
-    'SortBy': 'PARID',
-    'SortDir': ' asc',
-    'PageSize': '500',
-    'hdAction': 'Search',
-    'hdIndex': '',
-    'sIndex': '-1',
-    'hdListType': 'PA',
-    'hdJur': '',
-    'hdSelectAllChecked': 'false',
-    'inpParid': '1234',
-    'selSortBy': 'PARID',
-    'selSortDir': ' asc',
-    'selPageSize': '500',
-    'searchOptions$hdBeta': '',
-    'btSearch': '',
-    'RadWindow_NavigateUrl_ClientState': '',
-    'mode': 'PARID',
-    'mask': '',
-    'param1': '',
-    'searchimmediate': '',
+                    '__EVENTTARGET': '',
+                    '__EVENTARGUMENT': '',
+                    '__VIEWSTATE': view_state,
+                    '__VIEWSTATEGENERATOR': view_state_gen,
+                    '__EVENTVALIDATION': event_valid,
+                    'PageNum': '',
+                    'SortBy': 'PARID',
+                    'SortDir': ' asc',
+                    'PageSize': '500',
+                    'hdAction': 'Search',
+                    'hdIndex': '',
+                    'sIndex': '-1',
+                    'hdListType': 'PA',
+                    'hdJur': '',
+                    'hdSelectAllChecked': 'false',
+                    'inpParid': '1234',
+                    'selSortBy': 'PARID',
+                    'selSortDir': ' asc',
+                    'selPageSize': '500',
+                    'searchOptions$hdBeta': '',
+                    'btSearch': '',
+                    'RadWindow_NavigateUrl_ClientState': '',
+                    'mode': 'PARID',
+                    'mask': '',
+                    'param1': '',
+                    'searchimmediate': '',
 }
 
 
@@ -212,9 +210,9 @@ def myapi():
                         pass
                 for i in new:
                     current = "fourth"
-                    getData(i,Id['ASP.NET_SessionId'], agent,event_valid,view_state,view_state_gen)
+                    getData(i,event_valid,view_state,view_state_gen)
             else:
-                getData(query,Id['ASP.NET_SessionId'], agent,event_valid,view_state,view_state_gen)
+                getData(query,event_valid,view_state,view_state_gen)
     except Exception as e:
         return { "main":
                     {
