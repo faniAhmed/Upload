dc35654a6e33c025029c3dcc17529dc846ae006e
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-04-16 04:05:53 +0500

    Update fast.py

--- fast.py
+++ fast.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get('/')
 def myapi():
     agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0"
-    queries = ['01013']
+    queries = ['01000']
     try:
         current = "started"
         for query in queries:
