bdca667bd86c71c0792484c73fdd33467daf5cda
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-04-19 04:05:02 +0500

    Update fast.py

--- fast.py
+++ fast.py
@@ -8,7 +8,7 @@ def myapi():
     agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0"
     queries = ['01033']
     try:
-        current = "started"
+        
         for query in queries:
             def getData(query,event_valid,view_state,view_state_gen):
                 print(query)
